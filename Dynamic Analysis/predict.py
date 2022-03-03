03c90153265c8db57f25cb41a6960fb8638a841a
Suhas Thalanki <suhastvs@gmail.com>
2022-03-03 11:57:50 +0530

    started making necessary modifications to code, need to fix help message

--- Dynamic Analysis/predict.py
+++ Dynamic Analysis/predict.py
@@ -8,44 +8,18 @@ from sklearn.model_selection import train_test_split
 from collections import OrderedDict
 import time
 from collections import defaultdict
-
+import argparse
 from datetime import datetime,timedelta
 
-syscalls = dict()
-with open('syscall.csv') as f:
-    for line in f:
-        n,s = line.split(',')
-        n = int(n)
-        s = s.strip()
-        syscalls[s] = n
+# Creating argument parser
+desc = "Use the appropriate flags to choose the desired attack.\n\n Currently supported attacks are:\n1.Command Injection\n2.SQL Injection"
 
-filename = 'finalized_model_svm.sav'
-loaded_model = pickle.load(open(filename, 'rb'))
-
-# Load syscalls data into a dictionary
-syscalls = dict()
-with open('syscall.csv') as f:
-    for line in f:
-        n,s = line.split(',')
-        n = int(n)
-        s = s.strip()
-        syscalls[s] = n
-
-# Creates a vector of all the system calls
-columns = [j for i,j in syscalls.items()]
-current_time = None
-
-a='Attack'
-n='Normal'
-vec_type = int(input('enter 0 for normal, 1 for attack'))
-fn = a if vec_type==1 else n
-suffix = input('enter suffix digit')
-f = open("./Training/complete_vector/"+fn+suffix+".txt","r")
-l = f.readlines()
-f.close()
+parser = argparse.ArgumentParser(description=desc, add_help=False)
+parser.add_argument('-attack', help='Choose the attack to be predicted', type=str, required=True)
+parser.add_argument('--help','-h', action='help', help='An attack has to be specified with the -attack flag.\n\nCurrently supported attacks are:\n1.Command Injection\n2.SQL Injection\n\nThe attacks can be specified using the option:\n1. -attack command\n2. -attack sql')
 
 # Function for prediction
-def predict(freq, columns):
+def predict(freq, columns, loaded_model, count):
     # Create dataframe with required columns
     df_res = pd.DataFrame(columns=columns)
     # Add syscall frequency to respective columns
@@ -64,44 +38,86 @@ def predict(freq, columns):
         print(count, "\tNo attack")
         print(freq)
 
-freq = dict()
-count=1
-current_time = None
-columns = [j for i,j in syscalls.items()]
-for line in l:
-    # print('>>> {}'.format(line.decode('utf-8').rstrip()))
-    # print(line.decode('utf-8').rstrip().split(' '))
-    # t,s = line.decode('utf-8').strip('"|\n').split(' ')
-    # t,s = line.strip('"|\n').split(' ')
-    # s = s.strip()
-    line = line.rstrip()
-    # print(line)
-    t,s = line.split(' ')
-    # print(t,s)    
+def process(attack):
+    syscalls = dict()
+    with open('syscall.csv') as f:
+        for line in f:
+            n,s = line.split(',')
+            n = int(n)
+            s = s.strip()
+            syscalls[s] = n
 
-    # T is time, s is syscall
-    s = s.strip()
-    if s not in syscalls:
-        continue
-    t = t[:-3]
-    # Time of call being executed
-    dt = datetime.strptime(t,"%H:%M:%S.%f")
-    sn = syscalls[s]
-    # If first pass, get current time
-    if current_time is None:
-        current_time = dt
-    # If 100ms has not elapsed
-    if dt - current_time < timedelta(milliseconds=100):
-        if sn not in freq:
-            freq[sn] = 0
-        # Add frequency of the system call
-        freq[sn] += 1
-    # If 100 seconds has elapsed
-    else:
-        # Create thread and start as required
-        threading.Thread(target=predict, args=(freq, columns,)).start()
-        current_time = dt
-        # print(freq)
-        # print(df_res)
-        freq.clear()
-        count += 1
+    filename = 'finalized_model_svm.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    # Load syscalls data into a dictionary
+    syscalls = dict()
+    with open('syscall.csv') as f:
+        for line in f:
+            n,s = line.split(',')
+            n = int(n)
+            s = s.strip()
+            syscalls[s] = n
+
+    # Creates a vector of all the system calls
+    columns = [j for i,j in syscalls.items()]
+    current_time = None
+
+    a='Attack'
+    n='Normal'
+    vec_type = int(input('enter 0 for normal, 1 for attack'))
+    fn = a if vec_type==1 else n
+    suffix = input('enter suffix digit')
+    f = open("./Training/complete_vector/"+fn+suffix+".txt","r")
+    l = f.readlines()
+    f.close()
+
+    freq = dict()
+    count=1
+    current_time = None
+    columns = [j for i,j in syscalls.items()]
+    for line in l:
+        # print('>>> {}'.format(line.decode('utf-8').rstrip()))
+        # print(line.decode('utf-8').rstrip().split(' '))
+        # t,s = line.decode('utf-8').strip('"|\n').split(' ')
+        # t,s = line.strip('"|\n').split(' ')
+        # s = s.strip()
+        line = line.rstrip()
+        # print(line)
+        t,s = line.split(' ')
+        # print(t,s)    
+
+        # T is time, s is syscall
+        s = s.strip()
+        if s not in syscalls:
+            continue
+        t = t[:-3]
+        # Time of call being executed
+        dt = datetime.strptime(t,"%H:%M:%S.%f")
+        sn = syscalls[s]
+        # If first pass, get current time
+        if current_time is None:
+            current_time = dt
+        # If 100ms has not elapsed
+        if dt - current_time < timedelta(milliseconds=100):
+            if sn not in freq:
+                freq[sn] = 0
+            # Add frequency of the system call
+            freq[sn] += 1
+        # If 100 seconds has elapsed
+        else:
+            # Create thread and start as required
+            threading.Thread(target=predict, args=(freq, columns,loaded_model, count,)).start()
+            current_time = dt
+            # print(freq)
+            # print(df_res)
+            freq.clear()
+            count += 1
+
+if __name__ == "__main__":
+    options = ['Command', 'SQL']
+    args = parser.parse_args()
+    attack = args.attack
+    if attack not in options:
+        raise argparse.ArgumentTypeError(f'{attack} is not a valid option. Choose from {options}')
+    process(attack)
