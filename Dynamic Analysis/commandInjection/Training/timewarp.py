99bb332317e523d552bd54793d8593b0f742498b
Suhas Thalanki <suhastvs@gmail.com>
2022-04-01 12:44:17 +0530

    new script

--- Dynamic Analysis/commandInjection/Training/timewarp.py
+++ Dynamic Analysis/commandInjection/Training/timewarp.py
@@ -1,18 +1,24 @@
-from dtw import *
+from dtaidistance import dtw
+from dtaidistance import dtw_visualisation as dtwvis
+import numpy as np
 
 # Import first and second file
 
 file1 = open('./parsed_sysdig_data/attack_mounted_socket_1_workload_parsed.txt', 'r')
-file2 = open('./parsed_sysdig_data/attack_process_injection_1_workload_parsed.txt', 'r')
+file2 = open('./parsed_sysdig_data/attack_mounted_socket_2_workload_parsed.txt', 'r')
 
 f1 = list()
 for line in file1:
-    f1.append(line.split(',')[1])
+    f1.append(int(line.split(',')[1]))
+    
 
 f2 = list()
 for line in file2:
-    f2.append(line.split(',')[1])
+    f2.append(int(line.split(',')[1]))
 
-alignment = dtw(f1, f2, keep_internals=True)
+f1 = np.array(f1)
+f2 = np.array(f2)
+# print(type(f1[0]))
+path = dtw.warping_path(f1, f2)
 
-alignment.plot(type='threeway')
+dtwvis.plot_warping(f1, f2, path, filename='test1.png')
